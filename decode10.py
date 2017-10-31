252839d60322d591ea446a1ccac411ccb917b748
AbeLudlam <abeludlam@gmail.com>
2017-10-31 13:55:16 -0700

    Add files via upload

---
+++ decode10.py
@@ -0,0 +1,122 @@
+from PIL import Image
+
+#isEven is used to check the least significant bit of a pixel. Odd is 1, Even is 0.
+def isEven(val):
+	if val%2==0:
+		return True
+	else:
+		return False
+
+
+#Image input, and creating the pixel list.
+print("Enter the image name you wish to decode")
+ima = raw_input()
+im = Image.open(ima)
+
+pixels = list(im.getdata())
+width, height = im.size
+
+#read the last 10 bits and get the binary value of the text length.
+sto = ''
+for inc in range(1,11):
+	r, g, b = pixels[width*height-inc]
+	if isEven(r):
+		sto += '0'
+	else:
+		sto += '1'
+	if isEven(g):
+		sto += '0'
+	else:
+		sto += '1'
+	if inc != 10:
+		if isEven(b):
+			sto += '0'
+		else:
+			sto += '1'
+
+#Count is used to determine how many bits have been read, fin is the final decoded message, pos is used to track
+# which pixel is being checked, bi is used to store the 8 bits of a character before passing the ascii character
+# to fin, and gb is used to check how many of the three r,b,g values should be checked.
+count = 0
+fin = ''
+pos = 12
+bi = ''
+gb = 0
+
+#indicate that the text length indicates that there's to much text to read that the image does not have the room
+# to store. Stop from trying to read out of bounds.
+if (int(sto,2)*3) > (width*height):
+	print("Indicated file length is to large to fit in this image, please try encoding again")
+	quit()
+
+#For every character that should exist, while all 8 binary values have not been picked up, check to see if  
+# it needs to start with the green or blue values first, then check to see if it needs to finish after checking
+# the red or green values. Once all 8 bits have been taken, pass the decode ascii value to fin
+for onc in range(0, int(sto, 2)):
+	while count !=8:
+		if gb == 1:
+			r, g, b = pixels[width*height-pos]
+			if isEven(g):
+				bi += '0'
+			else:
+				bi += '1'
+			if isEven(b):
+				bi += '0'
+			else:
+				bi += '1'
+			pos = pos + 1
+			count = count + 2
+			gb = 0
+		elif gb == 2:
+			r, g, b = pixels[width*height-pos]
+			if isEven(b):
+				bi += '0'
+			else:
+				bi += '1'
+			pos = pos + 1
+			count = count + 1
+			gb = 0
+		elif count == 6:
+			r, g, b = pixels[width*height-pos]
+			if isEven(r):
+				bi += '0'
+			else:
+				bi += '1'
+			if isEven(g):
+				bi += '0'
+			else:
+				bi += '1'
+			count = count + 2
+			gb = 2
+		elif count == 7:
+			r, g, b = pixels[width*height-pos]
+			if isEven(r):
+				bi += '0'
+			else:
+				bi += '1'
+			count = count + 1
+			gb = 1
+		else:
+			r, g, b = pixels[width*height-pos]
+			if isEven(r):
+				bi += '0'
+			else:
+				bi += '1'
+			if isEven(g):
+				bi += '0'
+			else:
+				bi += '1'
+			if isEven(b):
+				bi += '0'
+			else:
+				bi += '1'
+			count = count + 3
+			pos = pos + 1
+		
+		if count == 8:
+			fin += chr(int(bi[:8], 2)) 
+			bi = ''
+	count = 0	
+					
+print(fin)			 
+
---
+++ encode10.py
@@ -0,0 +1,145 @@
+from PIL import Image
+
+#isEven is used to to determine the least significant bit for binary values. Even have 0, odd have 1.
+def isEven(val):
+	if val%2==0:
+		return True
+	else:
+		return False
+# File inputs and checks. fil is the file name, ima is the image name. Check to make sure there are enough bits
+# in the image to decode with
+print("Enter the file you wish to encode")
+fil = raw_input()
+with open(fil) as f:
+	tex = f.read()
+f.closed
+print("Enter the image name you wish to encode with")
+ima = raw_input()
+tmm = Image.open(ima)
+pixels = list(tmm.getdata())
+width, height = tmm.size
+if width*height <= len(tex)*3:
+	print("File is to large to fit into the picture, try again")
+	quit()
+	
+#fr is the binary string of the length of the text. Cut off the 0b at the start, and add 0 to the front to make it
+# the right size.
+fr = bin(len(tex))
+fr = fr[2:]
+for bn in range(0, 29-len(fr)):
+	fr = '0' + fr
+ad = 1
+#Look at the first 10 bits and change the red, blue and green pixels when necessary for decoding. If the binary of
+# the text length is 0, only change the bit value if its odd by subtracting one. Likewise, add a bit when the
+#binary value is odd and the bit is even. This prevents other bit changes. Pixel position is checked by the
+# value ad, while which r,g,b value is checked by the increment "on" mod 3.
+for on in range(0, 29):
+	r, g, b = pixels[width*height-ad]
+	if on%3 == 0:
+		if isEven(int(fr[on])):
+			if isEven(r) != True:
+				pixels[width*height-ad] = ((r-1), g, b)
+				
+			
+		else:
+			if isEven(r):
+				pixels[width*height-ad] = ((r+1), g, b)
+				
+		
+	if on%3 == 1:
+		if isEven(int(fr[on])):
+			if isEven(g) != True:
+				pixels[width*height-ad] = (r, (g-1), b)
+				
+		else:
+			if isEven(g):
+				pixels[width*height-ad] = (r, (g+1), b)
+				
+		
+	if on%3 == 2:
+		if isEven(int(fr[on])):
+			if isEven(b) != True:
+				pixels[width*height-ad] = (r, g, (b-1))
+				
+				
+		else:
+			if isEven(b):
+				pixels[width*height-ad] = (r, g, (b+1))
+				
+		ad += 1
+
+#The list i used was not changing the image, so load the image with pixe and change the pixels. widt is used
+# for the width position of and heigh for the height position
+pixe = tmm.load()
+heigh = 1
+widt = 1
+for sm in range(1,11):
+	if (width-widt) < 0:
+		he += 1
+		wi = 1
+	
+	pixe[width-widt, height-heigh] = pixels[width*height-sm]
+	widt +=1
+
+#for the actual text to binary encoding, we start by turning a character into binary, then going through each 0 
+# or 1, and changing the values the same way as the text length earlier. mn acts as our binary charracter holder
+# nm is our entire text in binary, being trimmed of its 0b headers, pos is used to know which r,g,b value to check
+# and pla is used to indicate which pixel is being changed.
+mn = "0"
+pos = 0
+pla = 12
+nm = ''.join(map(bin,bytearray(tex,'ascii')))
+
+for inc in range(0, len(tex)):
+	nm = ''.join(map(bin,bytearray(tex[inc],'ascii')))
+	for onc in range(0, 9-len(nm)):
+		mn += '0'
+	mn += nm[2:]
+	for eig in range(0,8):
+		r, g, b = pixels[width*height-pla]
+		if pos == 0:
+			
+			if isEven(int(mn[eig])):
+				
+				if isEven(r) != True:
+					pixels[width*height-pla]= ((r-1),g, b)
+			else: 
+				if isEven(r):
+					pixels[width*height-pla]= ((r+1),g, b)
+			pos += 1
+		elif pos == 1:
+			if isEven(int(mn[eig])):
+				if isEven(g) != True:
+					pixels[width*height-pla]= (r,(g-1), b)
+			else: 
+				if isEven(g):
+					pixels[width*height-pla]= (r,(g+1), b)
+			pos += 1
+		else:
+			if isEven(int(mn[eig])):
+				if isEven(b) != True:
+					pixels[width*height-pla]= (r,g, (b-1))
+			else: 
+				if isEven(b):
+					pixels[width*height-pla]= (r,g, (b+1))
+			pos = 0
+			pla += 1
+					 
+	
+	mn = '0'
+# Place the pixels into the image, wi and he are used for the actual pixel positions. It is run from 12 to
+#pla +1	because pla indicates the last pixel that was changed. The image is saved as the original 
+#image name +"-en.png"
+
+wi = 12
+he = 1
+for la in range(12, pla+1):
+	r, g, b = pixels[width*height-la]
+	if (width-wi) < 0:
+		he += 1
+		wi = 1
+	pixe[width-wi, height-he] = pixels[width*height-la]
+	wi += 1
+ima += '-en.png'
+tmm.save(ima, 'PNG')	
+	
